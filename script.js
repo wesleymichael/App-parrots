36fb4e06496aaa7443a07a8a16c689587db1460f
Wesley <wesleymichaelpereira@hotmail.com>
2023-01-11 21:19:02 -0300

    Efeito 3D da carta sendo virada

--- script.js
+++ script.js
@@ -13,12 +13,18 @@ function startGame(){
     //Array com as cartas
     //Temos um total de 7 pares
     
-    
     let listShuffled = selectDeck(nCards); 
+    backCards (listShuffled);
     
-    backCards (listShuffled);    
+    
+    //Agora o clique na carta. 
+    //Ao clicar na carta preciso fazer ela rotacionar com o efeito de rotação
+    //Inserir efeito 3D na carta
+    
+
 
 }
+
 
 
 
@@ -27,7 +33,7 @@ function selectDeck(nCards){
     let deck = [1, 2, 3, 4, 5, 6, 7].sort(comparador);        //Embaralhar o deck completo de cartas
     let listShuffled = deck.slice(0, nCards/2);               //Selecionar a quantidade de cartas selecionadas
     listShuffled = listShuffled.concat(listShuffled);         //Duplicar as cartas selecionadas para formar os pares
-    return listShuffled; 
+    return listShuffled.sort(comparador); 
 }
 
 /*** Função para distribuir as cartas sobre a 'mesa' ***/
@@ -40,68 +46,95 @@ function backCards (listShuffled){
         switch(listShuffled[i-1]){
             case 1:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/bobrossparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/bobrossparrot.gif" alt="Imagem não carregada">
+                    </div>
                 </div>`;
                 break;
             
             case 2:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/explodyparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/explodyparrot.gif" alt="Imagem não carregada">
+                    </div>
                 </div>`;
                 break;
             
             case 3:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/fiestaparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/fiestaparrot.gif" alt="Imagem não carregada">
+                    </div>   
                 </div>`;
                 break;
 
             case 4:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/metalparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/metalparrot.gif" alt="Imagem não carregada">
+                    </div>
                 </div>`;
                 break;
 
             case 5:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/revertitparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/revertitparrot.gif" alt="Imagem não carregada">
+                    </div>
                 </div>`;
                 break;
             
             case 6:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/unicornparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/unicornparrot.gif" alt="Imagem não carregada">
+                    </div>
                 </div>`;
                 break;
 
             case 7:
                 aux = `
-                <div class="card">
-                    <img src="img/back.png" alt="Imagem não carregada">
-                    <img src="img/tripletsparrot.gif" alt="Imagem não carregada">
+                <div onclick="turnCard(this)" class="card">
+                    <div class="front face">
+                        <img src="img/back.png" alt="Imagem não carregada">
+                    </div>
+                    <div class="back face">
+                        <img src="img/triplesparrot.gif" alt="Imagem não carregada">
+                    </div>
                 </div>`;
                 break;
         }
         table.innerHTML += aux;        
     }
-
-
-    console.log(table);
 }
 
-
+function turnCard(card){
+    card.classList.toggle('flip');
+}
 
 function comparador() { 
 	return Math.random() - 0.5; 
